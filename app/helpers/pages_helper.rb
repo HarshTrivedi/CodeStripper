6f531a5263e2dbd92b7cbd8e5279ebdcd8c6b5c9
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-07-28 12:41:18 +0530

    add language support for clojure , javascript , etc

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,8 +2,9 @@ module PagesHelper
 
 
 	def clear_code(language , code_string)
-		return clear_javalike_code(code_string) if language.include?("java") || language.include?("c") || language.include?("cpp")
+		return clear_javalike_code(code_string) if language.eql?("java") || language.eql?("c") || language.eql?("cpp") || language.eql?("scala") || language.eql?("java_script") || language.eql?("go")
 		return clear_rubylike_code(code_string) if language.include?("ruby")
+		return clear_clojurelike_code(code_string) if language.eql?("clojure")
 	end
 
 	def clear_javalike_code(code_string)
@@ -28,6 +29,17 @@ module PagesHelper
 		        complete_string = complete_string + line if not line.strip.chomp.empty?
 		end
 		complete_string.gsub(/(^=begin[\s\S]*^=end)/, "\r")
+	end
+
+	def clear_clojurelike_code(code_string)
+		complete_string = ""
+		code_string.split(/[\r]/).each do |line|
+				puts line
+		        line = line.gsub(/;.*/, "")
+				ap line
+		        complete_string = complete_string + line if not line.strip.chomp.empty?
+		end
+		return complete_string
 	end
 
 end
