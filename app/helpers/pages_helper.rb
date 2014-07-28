adb9946425b4d7e5f65b44a2727fc86fb83bce8f
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-07-28 15:44:09 +0530

    Add language support for php , html , css and python

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,6 +5,10 @@ module PagesHelper
 		return clear_javalike_code(code_string) if language.eql?("java") || language.eql?("c") || language.eql?("cpp") || language.eql?("scala") || language.eql?("java_script") || language.eql?("go")
 		return clear_rubylike_code(code_string) if language.include?("ruby")
 		return clear_clojurelike_code(code_string) if language.eql?("clojure")
+		return clear_phplike_code(code_string) if language.eql?("php")
+		return clear_htmllike_code(code_string) if language.eql?("html")
+		return clear_csslike_code(code_string) if language.eql?("css")
+		return clear_pythonlike_code(code_string) if language.eql?("python")
 	end
 
 	def clear_javalike_code(code_string)
@@ -14,7 +18,7 @@ module PagesHelper
 		        line = line.gsub(/(\/\/[\S \t]*$)/m , "")
 		        complete_string = complete_string + line if not line.strip.chomp.empty?
 		end
-		complete_string.gsub(/\/\*[\s\S]*\*\//m, "\r")
+		complete_string.gsub(/\/\*[\s\S]*?\*\//m, "\r")
 	end
 
 	def clear_rubylike_code( code_string)
@@ -28,7 +32,7 @@ module PagesHelper
 				end
 		        complete_string = complete_string + line if not line.strip.chomp.empty?
 		end
-		complete_string.gsub(/(^=begin[\s\S]*^=end)/, "\r")
+		complete_string.gsub(/(^=begin[\s\S]*?^=end)/, "\r")
 	end
 
 	def clear_clojurelike_code(code_string)
@@ -40,6 +44,46 @@ module PagesHelper
 		        complete_string = complete_string + line if not line.strip.chomp.empty?
 		end
 		return complete_string
+	end
+
+
+	def clear_phplike_code(code_string)
+		complete_string = ""
+		code_string.split(/[\r]/).each do |line|
+				ap line
+		        line = line.gsub(/(\/\/[\S \t]*$)/m , "")
+				line = line.gsub(/(#.*)/ , "")
+		        complete_string = complete_string + line if not line.strip.chomp.empty?
+		end
+		complete_string.gsub(/\/\*[\s\S]*?\*\//m, "\r")
+	end
+
+
+	#An HTML comment begins with "<!--", ends with "-->" and does not contain "--" or ">" anywhere in the comment.
+	def clear_htmllike_code(code_string)
+		complete_string = ""
+		code_string.split(/[\r]/).each do |line|
+		        complete_string = complete_string + line if not line.strip.chomp.empty?
+		end
+		complete_string.gsub(/(<!--[\s\S]*?-->)/, "\r")
+	end
+
+	def clear_csslike_code(code_string)
+		complete_string = ""
+		code_string.split(/[\r]/).each do |line|
+		        complete_string = complete_string + line if not line.strip.chomp.empty?
+		end
+		complete_string.gsub(/\/\*[\s\S]*?\*\//m, "\r")
+	end
+
+	def clear_pythonlike_code(code_string)
+		complete_string = ""
+		code_string.split(/[\r]/).each do |line|
+				ap line
+				line = line.gsub(/(#.*)/ , "")
+		        complete_string = complete_string + line if not line.strip.chomp.empty?
+		end
+		complete_string.gsub(/'''[\s\S]*?'''/, "\r")
 	end
 
 end
