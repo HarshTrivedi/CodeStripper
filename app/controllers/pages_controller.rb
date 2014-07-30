f9fd2411a7eae20ea00e43e34c5e935bf4f0013a
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-07-30 16:47:18 +0530

    change look and feel a little.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 		language = params[:language]
 		@cleared_code = clear_code(language , code_string)
 		@pretty_code = CodeRay.scan(@cleared_code, language).page.inspect
-		render :js => "$('#cleared_code').html(" + @pretty_code  + ");" + "document.body.style.paddingBottom = '150px';" + "document.body.style.paddingBottom = '150px';" + "document.body.style.background = '#FAFFFF' ;" + "document.body.style.paddingTop = '100px';"
+		render :js => "$('#cleared_code').html(" + @pretty_code  + ");" + "document.body.style.paddingBottom = '150px';" + "document.body.style.paddingBottom = '150px';" + "document.body.style.background = '#EBFAF9' ;" + "document.body.style.paddingTop = '100px';"
 	end
 
 end
